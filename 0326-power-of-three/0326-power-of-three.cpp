46de8837bce5fc61096905bc2fc16547d342ead9
Alwaysparth <165450049+Alwaysparth@users.noreply.github.com>
2026-08-10 10:59:22 +0530

    Time: 0 ms (100%), Space: 8.9 MB (44.2%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n%3==0){
+            n/=3;
+        }
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
